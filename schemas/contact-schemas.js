deccbc4dc06302a4d9e900d85a2ef7412cdc7177
Bohdan <112878985+Bohdan-FE@users.noreply.github.com>
2023-11-18 23:16:45 +0200

    finish hw-03

--- schemas/contact-schemas.js
+++ schemas/contact-schemas.js
@@ -1,6 +1,6 @@
 import Joi from 'joi'
 
-export const contactAddScheme = Joi.object({
+export const contactAddSchema = Joi.object({
     name: Joi.string().required().messages({
         'any.required': 'missing required name',
         'string.base': 'name must be string'
@@ -12,11 +12,17 @@ export const contactAddScheme = Joi.object({
     phone: Joi.string().required().messages({
         'any.required': 'missing required phone',
         'string.base': 'phone must be string'
-    })
+    }),
+    favorite: Joi.boolean()
 })
 
-export const contactUpdateScheme = Joi.object({
+export const contactUpdateSchema = Joi.object({
     name: Joi.string(),
     email: Joi.string(),
-    phone: Joi.string()   
+    phone: Joi.string(),
+    favorite: Joi.boolean()
+})
+ 
+export const contactFavoriteSchema = Joi.object({
+    favorite: Joi.boolean().required()
 })
