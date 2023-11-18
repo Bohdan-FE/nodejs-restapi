36272c5561f1785c2d776652ed2c5aef77da80e7
Bohdan <112878985+Bohdan-FE@users.noreply.github.com>
2023-11-18 18:28:39 +0200

    start

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
 import app from './app.js'
+import mongoose from 'mongoose'
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const { DB_HOSt }  = process.env
+
+mongoose.connect(DB_HOSt)
+  .then(() => {
+    console.log('Database connection successful')
+    app.listen(3000, () => {
+    console.log("Server running. Use our API on port: 3000")
+    })
+  })
+  .catch(error => {
+    console.log(error.message)
+    process.exit(1)
+  })
+
+
